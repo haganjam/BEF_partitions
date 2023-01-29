950028ce8bbb4f3a95a4e250c9955e3fe628aa33
James Hagan <james_hagan@outlook.com>
2023-01-29 19:23:31 +0100

    update the extension to pull into a function and split the abundance-based NBE into the three terms from fox

--- functions/02_petchey_2003_extension.R
+++ functions/02_petchey_2003_extension.R
@@ -9,118 +9,109 @@ source("functions/01_local_scale_partitions.R")
 install_if("dplyr")
 install_if("assertthat")
 
+# set-up a function for the Petchey (2003) extension
+petchey_2003_extension <- function(stock_data = NA,
+                                  fluxM_data, fluxY_data, 
+                                  RYe) {
+  
+  # sort out the fluxM_data
+  fluxM <- 
+    fluxM_data %>%
+    arrange(sample, species)
+  
+  # define expected relative yields
+  fluxM$RYe <- rep(RYe, n_unique(fluxM$sample))
+  
+  # get expected flux
+  fluxM$Mflux_exp <- fluxM$Mflux * fluxM$RYe
+  
+  # calculate the net biodiversity effect
+  Mflux_exp_tot_df <- aggregate(list(Yflux_exp_tot = fluxM[["Mflux_exp"]]), list(sample = fluxM$sample), sum)
+  fluxY_tot <- full_join(Mflux_exp_tot_df, fluxY_data , by = "sample")
+  
+  # if there are no biomass stock data, we calculate the NBE and return
+  if(!is.data.frame(stock_data)) {
+    
+    NBE_flux_tot <- 
+      fluxY_tot %>%
+      mutate(NBE_flux_tot = (Yflux-Yflux_exp_tot) ) %>%
+      select(sample, NBE_flux_tot)
+    
+    return(NBE_flux_tot)
+    
+  } 
+  
+  # join the stock data to the monoculture flux data
+  stock_fluxM <- full_join(stock_data, fluxM_data, by = c("sample", "species"))
+  
+  # calculate per biomass flux
+  stock_fluxM$fluxM_per_stock <- (stock_fluxM$Mflux/stock_fluxM$M)
+  
+  # calculate the expected flux in the mixture
+  stock_fluxM$Yflux_exp <- (stock_fluxM$Y*stock_fluxM$fluxM_per_stock)
+  
+  # calculate the expected mixture flux
+  fluxY_no_abun_df <- aggregate(list(Yflux_exp_no_abun = stock_fluxM[["Yflux_exp"]] ), list(sample = stock_fluxM$sample), sum)
+  fluxY_summary <- full_join(fluxY_no_abun_df, fluxY_tot, by = "sample")
+  
+  # calculate the different net biodiversity effects
+  NBE_flux <- 
+    fluxY_summary %>%
+    mutate(NBE_flux_tot = (Yflux - Yflux_exp_tot),
+           NBE_flux_no_abun = (Yflux - Yflux_exp_no_abun) )
+  
+  # calculate the NBE_flux_abun i.e. NBE flux due to changes in abundance
+  NBE_abun <- (NBE_flux$NBE_flux_tot - NBE_flux$NBE_flux_no_abun)
+  
+  # separate out the NBE_flux_abun term into contributions from Fox (2005)'s mechanisms
+  
+  # perform the fox partition
+  fox_part <- local_scale_part(data = stock_data, RYe = RYe, part = "fox_2005")
+  
+  # calculate the proportion of each of three terms in Fox (2005)'s partition
+  x <- apply(fox_part[,c("TI_CE", "TD_CE", "DOM")], 1, function(x) x/sum(x), simplify = FALSE )
+  y <- mapply(function(x, y) x*y, NBE_abun, x, SIMPLIFY = FALSE )
+  z <- bind_rows(y, .id = "sample")
+  names(z) <- c("sample", "NBE_flux_abun_TI.CE", "NBE_flux_abun_TD.CE", "NBE_flux_abun_DOM")
+  z$sample <- as.numeric(z$sample)
+  
+  # join to the NBE_flux data.frame
+  NBE_flux <- full_join(NBE_flux, z, by = "sample")
+  
+  # reorder the columns
+  NBE_flux <- 
+    NBE_flux %>%
+    select(sample, NBE_flux_tot, 
+           NBE_flux_abun_TI.CE, NBE_flux_abun_TD.CE, NBE_flux_abun_DOM,
+           NBE_flux_no_abun)
+  
+  return(NBE_flux)
+  
+  }
+
 # define some random examples
-
-# long-term change in biomass (Design 1a)
-data <- data.frame(sample = rep(c(1, 2), each = 2),
-                 species = rep(c(1, 2), 2),
-                 M = rep(c(500, 250), 2),
-                 Y = c(300, 100, 330, 110))
-
-RYe <- c(0.5, 0.5)
-
-# in combination with Design 1a or with Design 2
-
-# calculate per biomass flux measurements then generate the expectation
-# from the mixture biomass data
-local_scale_part(data = f1.biomass, RYe = c(0.5, 0.5), part = "fox_2005")
-
-# define some other function data
+stock_data <- data.frame(sample = rep(c(1, 2), each = 2),
+                         species = rep(c(1, 2), 2),
+                         M = rep(c(500, 250), 2),
+                         Y = c(300, 100, 330, 110))
 
 # monoculture data
-f1.flux.M <- data.frame(sample = rep(c(1, 2), each = 2),
-                        species = rep(c(1, 2), 2),
-                        MF = c(80, 50, 100, 50))
+fluxM_data <- data.frame(sample = rep(c(1, 2), each = 2),
+                         species = rep(c(1, 2), 2),
+                         Mflux = c(80, 50, 100, 50))
 
 # mixture
-f1.flux.Y <- data.frame(sample = c(1, 2),
-                        YF = c(80, 90))
+fluxY_data <- data.frame(sample = c(1, 2),
+                         Yflux = c(80, 90))
 
+# test the function without stock data
+petchey_2003_extension(stock_data = NA,
+                       fluxM_data = fluxM_data, fluxY_data = fluxY_data, 
+                       RYe = c(0.5, 0.5))
 
+# test the function with stock data
+petchey_2003_extension(stock_data = stock_data,
+                       fluxM_data = fluxM_data, fluxY_data = fluxY_data, 
+                       RYe = c(0.5, 0.5))
 
-
-
-# get the number of species in the data
-n_sp <- n_unique(data$species)
-
-# sort the data.frame
-df <- 
-  data %>%
-  arrange(sample, species)
-
-# define expected relative yields
-df$RYe <- rep(RYe, n_unique(df$sample))
-
-# define observed relative yields: Prevent dividing by zero
-df$RYo <- ifelse(df$M == 0, 0, (df$Y/df$M))
-
-# define the change in relative yield
-df$dRY <- (df$RYo - df$RYe)
-
-# calculate expected yield for each species
-df$Ye <- (df$M*df$RYe)
-
-# check the observed and expected yields
-df %>%
-  group_by(sample) %>%
-  summarise(Ye = sum(Ye),
-            Y = sum(Y))
-
-# so, biomass increases by 25 and 65 units respectively
-
-# how much increase in flux should this be as measured in the standard way
-
-Exp.flux <- 
-  f1.flux.M %>% 
-  mutate(RYe = rep(RYe, 2)) %>%
-  mutate(MF_Exp = (MF*RYe)) %>%
-  group_by(sample) %>%
-  summarise(MF_Exp = sum(MF_Exp))
-print(Exp.flux)
-
-# calculate the total NBE of the fluxes
-NBE_flux <- 
-  full_join(Exp.flux, f1.flux.Y, by = "sample") %>%
-  mutate(NBE_flux_total = YF - MF_Exp)
-
-# so that's the total difference in flux rates between mono and mixture
-NBE_flux  
-
-# we parse out any effects due to changing abundance
-f1.flux.Y.Exp <- full_join(f1.biomass, f1.flux.M, by = c("sample", "species") )
-print(f1.flux.Y.Exp)
-
-# calculate per biomass flux
-f1.flux.Y.Exp <- 
-  f1.flux.Y.Exp %>%
-  mutate(MF_per_bio = MF/M)
-print(f1.flux.Y.Exp )
-
-# calculate the expected flux in the mixture
-f1.flux.Y.Exp <- 
-  f1.flux.Y.Exp %>%
-  mutate(Y_Exp = Y*MF_per_bio) %>%
-  group_by(sample) %>%
-  summarise(Y_Exp = sum(Y_Exp))
-print(f1.flux.Y.Exp )
-
-# join the expected flux to the true flux data
-f1.flux <- full_join(f1.flux.Y.Exp, f1.flux.Y, by = "sample")
-print(f1.flux)
-
-f1.flux <- 
-  f1.flux %>%
-  mutate(NBE_flux_no_abun = YF-Y_Exp)
-
-# join these together
-full_join(NBE_flux, f1.flux, by = "sample") %>%
-  mutate(NBE_flux_abun = (NBE_flux_total - NBE_flux_no_abun) ) %>%
-  select(sample, NBE_flux_total, NBE_flux_abun, NBE_flux_no_abun)
-
-
-local_scale_part(data = f1.biomass, RYe = c(0.5, 0.5), part = "fox_2005")
-
-
-
-
-
+### END
